22c26434d0964137becea119e8b7c68c227c3709
k.kolesnikov <k.kolesnikov@profsoft.pro>
2022-11-07 13:46:03 +0400

    Initial Commit

---
+++ ColorPalette/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ColorPalette
+//
+//  Created by Кирилл Колесников on 07.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
