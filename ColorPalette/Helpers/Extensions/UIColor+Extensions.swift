79171ab012e2bf9e10bf9782d83024800b1234ac
k.kolesnikov <k.kolesnikov@profsoft.pro>
2023-01-13 15:20:09 +0400

    Add InclusiveColor; change UIColor+Extensions

--- ColorPalette/Helpers/Extensions/UIColor+Extensions.swift
+++ ColorPalette/Helpers/Extensions/UIColor+Extensions.swift
@@ -9,6 +9,8 @@ import Foundation
 import SwiftUI
 import UIKit
 
+typealias RGBA = (red: Int, green: Int, blue: Int, alpha: Int)
+
 extension Color {
     init(_ appColor: AppColor) {
         self.init(uiColor: UIColor(appColor))
@@ -232,5 +234,46 @@ extension UIColor {
             blue: .random(in: 0...1),
             alpha: 1.0
         )
+    }
+}
+
+extension UIColor {
+    
+    /// Returns the color simulated to appear as though viewed by an individual afflicted with a specified type of color blindness.
+    ///
+    /// - Parameter type: The type of colorblindness for which to simulate the color.
+    /// - Returns: The color simulated to appear as though viewed by an individual afflicted with a specified type of color blindness.
+    func inclusiveColor(for type: InclusiveColor.BlindnessType) -> UIColor {
+        let inclusiveColor = InclusiveColor()
+        guard let color = rgba() else { fatalError("Attempted to simulate the appearance of an unspecified color.") }
+        
+        switch type {
+            case .normal:
+                return inclusiveColor.rgbToUIColor(rgb: color)
+            case .protanopia:
+                return inclusiveColor.rgbToUIColor(rgb: inclusiveColor.blindMK(rgb: color, deficiency: .protan))
+            case .protanomaly:
+                return inclusiveColor.rgbToUIColor(rgb: inclusiveColor.anomylize(rgb: color, adjustedRGB: (inclusiveColor.blindMK(rgb: color, deficiency: .protan))))
+            case .deuteranopia:
+                return inclusiveColor.rgbToUIColor(rgb: inclusiveColor.blindMK(rgb: color, deficiency: .deutan))
+            case .deuteranomaly:
+                return inclusiveColor.rgbToUIColor(rgb: inclusiveColor.anomylize(rgb: color, adjustedRGB: (inclusiveColor.blindMK(rgb: color, deficiency: .deutan))))
+            case .tritanopia:
+                return inclusiveColor.rgbToUIColor(rgb: inclusiveColor.blindMK(rgb: color, deficiency: .tritan))
+            case .tritanomaly:
+                return inclusiveColor.rgbToUIColor(rgb: inclusiveColor.anomylize(rgb: color, adjustedRGB: (inclusiveColor.blindMK(rgb: color, deficiency: .tritan))))
+            case .achromatopsia:
+                return inclusiveColor.rgbToUIColor(rgb: inclusiveColor.monochrome(rgb: color))
+            case .achromatomaly:
+                return inclusiveColor.rgbToUIColor(rgb: inclusiveColor.anomylize(rgb: color, adjustedRGB: (inclusiveColor.monochrome(rgb: color))))
+        }
+    }
+    
+    func rgba() -> RGBA? {
+        var redLiteral = CGFloat(0.0), greenLiteral = CGFloat(0.0), blueLiteral = CGFloat(0.0), alphaLiteral = CGFloat(0.0)
+        
+        guard getRed(&redLiteral, green: &greenLiteral, blue: &blueLiteral, alpha: &alphaLiteral) else { assertionFailure("Could not extract RGBA components"); return nil }
+        
+        return (red: Int(redLiteral * 255), green: Int(greenLiteral * 255), blue: Int(blueLiteral * 255), alpha: Int(alphaLiteral) * 255)
     }
 }
